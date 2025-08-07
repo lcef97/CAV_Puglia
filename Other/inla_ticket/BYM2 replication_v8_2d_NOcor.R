781541a95782ab3d702cee992e6c33a68fa11787
Leonardo Cefalo <leonardo.cefalo@uniba.it>
2025-08-07 19:15:38 +0200

    NO correlation

---
+++ Other/inla_ticket/BYM2 replication_v8_2d_NOcor.R
@@ -0,0 +1,152 @@
+##' Simulate BYM model ---------------------------------------------------------
+
+#' Neighbourhood matrix  ==> binary
+load(url("https://github.com/lcef97/CAV_Puglia/raw/main/input/Replication/W.RData"))
+#' Source functions to implement models; 
+#' Link to R code: 'https://github.com/lcef97/CAV_Puglia/blob/main/Auxiliary/Functions.R'
+devtools::source_url("https://raw.githubusercontent.com/lcef97/CAV_Puglia/refs/heads/main/Auxiliary/Functions.R")
+
+n <- nrow(W)
+#' number of variables --------------------------------------------------------#
+k <- 2
+#' Hyperparameters ------------------------------------------------------------#
+phi.true <- c(0.95, 0.4)
+sigma.true <- c(1.2, 0.8)
+#' Independent fields this time! ----------------------------------------------#
+corr.true <- 0
+Scale.true <- varcov(c(sigma.true, corr.true), tri=F)
+M.true <- diag(sqrt(eigen(Scale.true)$values)) %*% t(eigen(Scale.true)$vectors)
+
+#' Add covariate --------------------------------------------------------------#
+set.seed(11)
+beta0 <- 2
+
+#' Simulate BYM field ---------------------------------------------------------#
+L.unscaled <- Matrix::Diagonal(rowSums(W), n=n) - W
+L <- INLA:::inla.scale.model.bym.internal(L.unscaled)$Q
+constr <- INLA:::inla.bym.constr.internal(L)
+constr.BYM <- list(A = cbind(Matrix::Matrix(0, nrow=k, ncol = k*n), 
+                             kronecker(diag(k), constr$constr$A)),
+                   e = rep(0, k))
+#' Change seed!
+set.seed(11)
+#' IID component
+V.true <- matrix(rnorm(n=k*n, mean=0, sd=1), nrow = n, ncol = k, byrow=F)
+#' ICAR component
+U.lst <- lapply(c(1:k), function(i) {
+  MASS::mvrnorm(n=1, mu=rep(0,n), Sigma=INLA:::inla.ginv(L)+Matrix::Diagonal(n=n, x=1e-7))
+  })
+U.true <- scale(do.call(cbind, U.lst))
+#' BYM2 process - weighted sum of IID and ICAR, then scaled by M  
+Z.true <- as.vector(U.true %*% diag(sqrt(phi.true)) %*% M.true + 
+  V.true %*% diag(sqrt(1 - phi.true)) %*% M.true)
+
+#' Simulation of the observed variable ----------------------------------------#
+y.sim <- numeric(length(Z.true))
+for(i in seq(length(y.sim))) {
+  eta <- beta0 + Z.true[i]
+  y.sim[i] <- rpois(n=1, lambda = exp(eta) )
+}
+
+dd <- data.frame(Y = y.sim, ID = c(1:(k*n)), group=rep(c(1:k), each=n))
+
+##' Toy examples: simpler models -----------------------------------------------
+
+#' Easiest: IMCAR -------------------------------------------------------------#
+#' 
+#' Not the true model, but very easy to fit.
+#' Covariances matrix has Wishart(2k, I_k) prior. 
+#' If Bartlett factorisation is used, all IMCAR, PMCAR and LMCAR work well.
+
+mod.IMCAR <- inla(
+  Y ~ 1 + 
+    f(ID, model = inla.IMCAR.Bartlett(k = 2, W = W, df=4, scale.model = T ),
+      extraconstr = list(A=constr.BYM$A[,-c(1:(k*n))], e = rep(0, k)) ),
+  family = "poisson", data = dd, num.threads = 1, 
+  control.inla = list(tolerance=1e-7, h=1e-5),
+  control.compute = list(internal.opt = F, cpo = T, waic = T, config = T), 
+  verbose = T)
+
+#' Hyperparameters posterior.
+#' Variance ==> Overestimated, like 2x  (not true DGP...)
+vcov_summary(mod.IMCAR)$var
+#' Correlations: low, indeed. This is good
+vcov_summary(mod.IMCAR)$cor
+ 
+
+#' BYM TOY example: independent fields ----------------------------------------#
+#'
+#' Ok this is literally the true DGP 
+#' sum-to-zero constraint on the ICAR component and NOT on the convolution component,
+#' as it appears to be in the univariate version implemented with `model='bym2'` 
+#' ...need a more strict prior on the mixing
+mod.INDMMBYM  <- inla(
+  Y ~ 1+
+    f(ID, model = inla.INDMMBYM.model(k = 2, W = W, df=4, PC = T, alpha = 0.99, U = 0.5 ),
+      extraconstr = constr.BYM),
+  family = "poisson", data = dd, num.threads = 1,
+  control.compute = list(internal.opt = F, cpo = T, waic = T, config = T), 
+  verbose = T)
+# Mixing parameter ==> perfect
+Mmodel_compute_mixing(mod.INDMMBYM) 
+#' Variances ==> overestimated the first one
+data.frame( do.call(rbind, lapply(
+  lapply(mod.INDMMBYM$marginals.hyperpar[-c(1:k)], function(f){
+    inla.tmarginal(fun = function(X) exp(-X), marginal = f)
+  }), function(x) unlist(inla.zmarginal(x, silent = TRUE)))))[,c(1,2,3,5,7)]
+
+
+##' multivariate BYM - where trouble begins ------------------------------------
+ 
+#' A priori, again, var-cov ~ Wishart(2k, I_k), while the mixing parameter
+#' follows a multivariate Uniform distribution 
+#' WARNING: slow - falls into `Enable early_stop`` 
+mod.MMBYM <- inla(
+  Y ~ 1+ f(ID, model = inla.MMBYM.model(
+    k = 2, W = W, df=4 ),
+      extraconstr = constr.BYM),
+  family = "poisson", data = dd, num.threads = 1,
+  control.inla = list(tolerance = 1e-7),
+  control.compute = list(internal.opt = F, cpo = T, waic = T, config = T), 
+  verbose = T)
+  
+
+#' Correlations: definitively out of the way; variance: messy.
+vcov_summary(mod.MMBYM)
+#' mixing: basically inverted
+Mmodel_compute_mixing(mod.MMBYM)
+ 
+
+
+#' What would've been with initial values based on ICAR output ----------------#
+
+mod.MMBYM.guided <- inla(
+  Y ~ 1+  f(ID, model = inla.MMBYM.model(
+    k = 2, W = W, df=4,    
+    initial.values = c(rep(-3, 2), mod.IMCAR$summary.hyperpar$mode)),
+      extraconstr = constr.BYM),
+  family = "poisson", data = dd, num.threads = 1,
+  #control.inla = list(tolerance = 1e-7),
+  control.compute = list(internal.opt = F, cpo = T, waic = T, config = T), 
+  verbose = T)
+
+#' Correlations: perfect (it's the true DGP)
+#' Variances: overestimated as usual
+vcov_summary(mod.MMBYM.guided)
+#' Mixing: overestimated
+Mmodel_compute_mixing(mod.MMBYM.guided)
+
+#' Mode comparison ------------------------------------------------------------#
+#' 
+#' Core issue: same priors, same data yet the mode seems different:
+
+#' -2 and 0 as starting log-standard deviation and correlation:
+mod.MMBYM$mode$log.posterior.mode
+mod.MMBYM$misc$configs$max.log.posterior
+#' low
+
+#' starting values from ICAR:
+mod.MMBYM.guided$mode$log.posterior.mode
+mod.MMBYM.guided$misc$configs$max.log.posterior
+#' high
+
